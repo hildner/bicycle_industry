f0d8a9c2ced2876fbc8888a876ee7712611c159b
Hildner <christopher.hildner@gmail.com>
2016-08-03 15:12:31 +0000

    Initial Commit

--- bicycle.py
+++ bicycle.py
@@ -49,22 +49,6 @@ def affordable_bikes(models, customers):
     
     return affordable
 
-#("Model Name", weight, production cost, inventory)
-models = Bicycle("X100", 20, 100, 25)
-models = Bicycle("X200", 15, 300, 23)
-models = Bicycle("X300", 12, 400, 21)
-models = Bicycle("X400", 10, 600, 19)
-models = Bicycle("X600", 8, 800, 12)
-
-
-customers = Customer("Alex",200)
-customers = Customer("Beth",500)
-customers = Customer("Charles",1000)
-
-
-shop = Shops(models)
-options = affordable_bikes(models,customers)
-shop.sell_bikes(models,customers,options)
 
 
 
